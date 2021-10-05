17151a6e54ec6e52ebce53a3a4afaf0e61e15d2e
Tyler <tyler.wong@ontariotechu.net>
2021-10-05 13:21:31 -0400

    Starting character controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public CharacterController controller;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    public float moveSpeed = 6f;
+
+    Vector3 direction;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+        direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            controller.Move(direction * moveSpeed * Time.deltaTime);
+        }
     }
 }
