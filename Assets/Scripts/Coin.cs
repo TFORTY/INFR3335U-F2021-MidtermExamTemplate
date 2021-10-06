5009ba7fb6639dc44feed8dc9fb205a0f488662c
Tyler <tyler.wong@ontariotechu.net>
2021-10-05 20:46:31 -0400

    Made coins rotate

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Coin : MonoBehaviour
+{
+    public float rotateSpeed = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotateSpeed * Time.deltaTime, 0f, 0f);
+    }
+}
