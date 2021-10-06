8e56f3ccf039df9232daeb9cebaa83072baf0e2f
Tyler <tyler.wong@ontariotechu.net>
2021-10-06 13:47:32 -0400

    Commented code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -37,10 +37,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Gets the direction of the player based in their input
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
         direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
 
+        // Rotates the player to face the direction they are moving in
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
@@ -56,11 +58,13 @@ public class PlayerController : MonoBehaviour
             isWalking = false;
         }
 
+        // Changes to the walking animation
         if (isWalking)
         {
             GetComponent<Animator>().SetInteger("AnimatorState", 1);
             timer = 0;
         }
+        // Changes to the idle animation
         else
         {
             timer += Time.deltaTime;
@@ -71,6 +75,8 @@ public class PlayerController : MonoBehaviour
             }
         } 
 
+        // Checks if the player has collected all coins. 
+        // If so, switches to the end screen
         if (coinCount == 10)
         {
             SceneManager.LoadScene("End");
@@ -79,16 +85,17 @@ public class PlayerController : MonoBehaviour
         CoinsNeeded();
     }
 
+    // Checks if the player has collided with a coin and adds to their total coin count
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Coin")
         {
-            Debug.Log("COIN");
             Destroy(other.gameObject);
             coinCount++;
         }
     }
 
+    // Updates text to let player know how many coins they have collected
     void CoinsNeeded()
     {
         coinsLeft.text = coinCount.ToString("");
